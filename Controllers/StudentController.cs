74eb838737d3168d8d2d7e63a307ed25ad2d2569
Ömer Erdem BİLGİN <o.erdem.bilgin@icisleri.gov.tr>
2025-03-06 16:35:01 +0300

    Created Edit Student Functionality

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -31,5 +31,57 @@ namespace EfcoreApp.Controllers
             await _context.SaveChangesAsync();
             return RedirectToAction("Index");
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Student? student = await _context.Students.FindAsync(id);
+
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return View(student);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Student model)
+        {
+            if (id != model.StudentId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(model);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!_context.Students.Any(s => s.StudentId == model.StudentId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
     }
 }
